519d5cf1f6ae156f3a97efe22333de649fa22d78
Jorge Cotillo <jorge.cotillo@gmail.com>
2017-01-19 03:56:40 -0500

    Modified files to be able run dotnet core 1.1.0. Updated webpack-hot-middleware version to 2.15.0. Added custom port.

--- Angular2Application2/Program.cs
+++ Angular2Application2/Program.cs
@@ -21,6 +21,7 @@ namespace Angular2Application2
                 .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls("http://*:4040")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
